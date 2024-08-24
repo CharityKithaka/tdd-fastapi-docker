b6d408da729a8d846c3ae7a4d42e7b9f4eae6b73
Charity K <charity.kithaka@simplexi.io>
2024-08-24 14:53:36 +0300

    Pytest

---
+++ tests/test_ping.py
@@ -0,0 +1,10 @@
+from app import main
+
+
+def test_ping(test_app):
+    #Given we have a test app
+    #when we send a get
+    response = test_app.get("/ping")
+    #then we should get a 200 response
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
